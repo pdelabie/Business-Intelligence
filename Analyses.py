1aaa6cb10b43de6366ffb94ac778c7f95f1cbf35
pdelabie <101878906+pdelabie@users.noreply.github.com>
2022-03-18 16:17:54 +0100

    Update Analyses.py

--- Analyses.py
+++ Analyses.py
@@ -25,3 +25,51 @@ selectbox = st.selectbox(
 st.write(f"You selected {selectbox}")
 #The first argument to st.selectbox is the string to display and the second argument is a list of options to select. 
 #And then we display the selected value in the app using st.write method.
+#The first argument to st.selectbox is the string to display and the second argument is a list of options to select. 
+#And then we display the selected value in the app using st.write method.
+#Checkbox in streamlit
+st.title("Welcome to Streamlit! Checkbox")
+
+checkbox_one = st.checkbox("Yes")
+checkbox_two = st.checkbox("No")
+
+if checkbox_one:
+    value = "Yes"
+elif checkbox_two:
+    value = "No"
+else:
+    value = "No value selected"
+
+st.write(f"You selected: {value}")
+
+#Line chart in streamlit
+import numpy as np
+st.title("Welcome to Streamlit! Line chart")
+st.write("Line Chart in Streamlit")
+# 10 * 2 dimensional data
+chart_data = pd.DataFrame(
+    np.random.randn(10, 2),
+    columns=[f"Col{i+1}" for i in range(2)]
+)
+
+st.line_chart(chart_data)
+#Plotly charts in streamlit
+#pip install plotly
+import plotly.graph_objects as go
+
+st.title("Welcome to Streamlit! Plotly Graphs")
+
+fig = go.Figure(
+    data=[go.Pie(
+        labels=['A', 'B', 'C'],
+        values=[30, 20, 50]
+    )]
+)
+fig = fig.update_traces(
+    hoverinfo='label+percent',
+    textinfo='value',
+    textfont_size=15
+)
+
+st.write("Pie chart in Streamlit")
+st.plotly_chart(fig)
