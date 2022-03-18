5d946f823429268d3fc591310a9f4952715b1674
pdelabie <101878906+pdelabie@users.noreply.github.com>
2022-03-18 16:02:16 +0100

    Update Analyses.py

--- Analyses.py
+++ Analyses.py
@@ -2,3 +2,16 @@ import streamlit as st
 #Widgets
 value = st.slider('val')  # this is a widget
 st.write(value, 'squared is', value * value)
+#Layout
+#Streamlit runs the python script from top to bottom
+#Each time the user interacts the script is a rerun from top to bottom
+#Streamlit allows you to use caching for costly operations like loading large datasets
+import pandas as pd
+st.title("Welcome to Streamlit! DataFrame")
+st.write("Our first DataFrame")
+st.write(
+  pd.DataFrame({
+      'A': [1, 2, 3, 4],
+      'B': [5, 6, 7, 8]
+    })
+)
